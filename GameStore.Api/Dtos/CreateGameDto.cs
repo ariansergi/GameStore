16b0208f67ab4347ece7ba4af2445be780084a8c
ariansergi <arian.sergi@gmail.com>
2024-05-15 17:20:28 +0100

    first commit

---
+++ GameStore.Api/Dtos/CreateGameDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Api.Dtos;
+
+public record class CreateGameDto(
+    [Required]
+    [StringLength(50, MinimumLength = 3)]
+    string Name, 
+    int GenreId,
+    [Range(1, 999.99)]
+    decimal Price, 
+    DateTime ReleaseDate
+);
---
+++ GameStore.Api/Dtos/UpdateGameDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Api.Dtos;
+
+public record class UpdateGameDto (
+    [Required]
+    [StringLength(50, MinimumLength = 3)]
+    string Name, 
+    [Required]
+    int GenreId, 
+    [Required]
+    [Range(1, 999.99)]
+    decimal Price, 
+    DateTime ReleaseDate
+);
